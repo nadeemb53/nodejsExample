308aed274d1a3f9bf00dfdec160a02c8925080ac
Nadeem Bhati <nadeemb53@gmail.com>
2018-08-09 22:58:57 +0530

    callbacks and error handling

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var rect = require('./rectangle');
 
 function solveRect(l,b){
     console.log("Solving for rect with l="+l+" and b="+b);
-    if(l<0 || b<0){
-        console.log("Shouldn't be negative");
-    }
-    else{
-        console.log("Area of Rectangle= "+rect.area(l,b));
-        console.log("Perimeter of Rectangle= "+rect.perimeter(l,b));
-    }
+   rect(l,b, (err, rectangle)=> {
+       if(err){
+           console.log("ERROR: "+err.message);
+       }
+       else{
+           console.log("Perimeter="+rectangle.perimeter());
+           console.log("Area="+rectangle.area());
+       }
+   });
+   console.log("this statement after the call to rect()");
 }
 
 solveRect(12,48);
